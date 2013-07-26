ad163dcdc4808841096abe340d4400cd2ebcc665
Timothy Miller <howlermiller@gmail.com>
2013-07-26 15:36:29 -0500

    Whoops, I broke stuff before. Now it's fixed.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-var c = document.getElementById("c");
-var ctx = c.getContext('2d');
-
-var wwidth = $(window).width();
-var wheight = $(window).height();
+var c = document.getElementById("c")
+,ctx = c.getContext('2d')
+,wwidth = $(window).width()
+,wheight = $(window).height()
+,widthIsBiggerThanHeight = (wwidth > wheight) ? true : false
+,xHeight = ((widthIsBiggerThanHeight) ? wheight * 0.75 : wwidth * 0.75)
+;
 
 c.width = wwidth;
 c.height = wheight;
 
-
-ctx.font = "bold 300px Helvetica";
-ctx.fillText("x", 200, 200);
+ctx.fillStyle = "#FB0D0B";
+ctx.font = "bold " + xHeight + "px Helvetica";
+ctx.textAlign = "center";
+ctx.fillText("x", wwidth / 2, wheight / 2 + xHeight / 4);
