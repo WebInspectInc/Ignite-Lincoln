708e047d7ef559fd5de012e58f8fe5cbfd630639
Timothy Miller <howlermiller@gmail.com>
2013-07-26 16:37:09 -0500

    Fully merged and working (as far as I know)

--- js/index.js
+++ js/index.js
@@ -1,29 +1,4 @@
-//@codekit-prepend "ParticleSystem.js";
-
-(function() {
-    var lastTime = 0;
-    var vendors = ['webkit', 'moz'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame =
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+//@codekit-prepend "convenience.js", ParticleSystem.js";
 
 var c = document.getElementById("c")
 	,ctx = c.getContext('2d')
@@ -68,9 +43,6 @@ function findWidth(intendedWidth, someText)
 		test, fin, dir,
 		end = false;
 
-	console.log("ha");
-	console.log(someText);
-
 	while(end == false)
 	{
 		ctx.font = size + "px Helvetica";
@@ -96,16 +68,14 @@ function findWidth(intendedWidth, someText)
 }
 
 var particleSystem = new ParticleSystem(ctx);
-particleSystem.x = wwidth / 2;
-particleSystem.y = wheight / 2;
+particleSystem.x = wWidth / 2;
+particleSystem.y = wHeight / 2;
 
 function draw()
 {
-	ctx.clearRect(0, 0, wwidth, wheight);
-	ctx.fillStyle = "#FB0D0B";
-	ctx.font = "bold " + xHeight + "px Helvetica";
-	ctx.textAlign = "center";
-	ctx.fillText("x", wwidth / 2, wheight / 2 + xHeight / 4);
+	ctx.clearRect(0, 0, wWidth, wHeight);
+	drawX();
+	drawMotto();
 	particleSystem.draw();
 }
 
